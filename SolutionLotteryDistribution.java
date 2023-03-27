0d3f701342f33b56e3e98e31cea2f2179010a25d
Ademba <oscarademba+github@gmail.com>
2023-03-27 11:34:57 +0300

    Update and rename Solution.java to SolutionLotteryDistribution.java

--- SolutionLotteryDistribution.java
+++ SolutionLotteryDistribution.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class LotteryDistribution {
+public class SolutionLotteryDistribution {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
