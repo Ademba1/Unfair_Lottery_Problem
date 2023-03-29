c245b10af859b85cd355bb7ab2ab652112cb07aa
Ademba <oscarademba+github@gmail.com>
2023-03-29 10:50:27 +0300

    Update UnfairLottery.py

--- UnfairLottery.py
+++ UnfairLottery.py
@@ -1,9 +1,9 @@
-def reverse(arr):
-    i, j = 0, len(arr) - 1
-    while i < j:
-        arr[i], arr[j] = arr[j], arr[i]
-        i += 1
-        j -= 1
+# def reverse(arr):
+#     i, j = 0, len(arr) - 1
+#     while i < j:
+#         arr[i], arr[j] = arr[j], arr[i]
+#         i += 1
+#         j -= 1
 
 
 def get_winner_with_lowest_prize(prize_map):
